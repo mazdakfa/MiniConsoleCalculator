cb96ee80d51086e6998a4c44445447ed75f91e66
Mazdak <mzdk60@gmail.com>
2026-02-08 23:54:35 +0330

    HistoryService methods completed

--- src/main/java/com/mazdak/HistoryService.java
+++ src/main/java/com/mazdak/HistoryService.java
@@ -1,4 +1,23 @@
 package com.mazdak;
 
+import java.util.ArrayList;
+
 public class HistoryService {
+    private static ArrayList<String> history = new ArrayList<>();
+    public void add(String item){
+        history.add(item);
+    }
+    public void show(){
+        if (history.isEmpty())
+            throw new InvalidOperationException("History is empty; there is nothing to display.");
+        int i=1;
+        for (String item:history){
+            System.out.printf("%3d) %s%n",i,item);
+            i++;
+        }
+    }
+    public void clear(){
+        history.clear();
+        System.out.println("History completely cleared.");
+    }
 }
