770ac845c04d62f55e72774be087df6c965722c3
Mazdak <mzdk60@gmail.com>
2026-02-08 11:38:13 +0330

    The main loop and the basis of the application

--- src/main/java/com/mazdak/CalculatorApp.java
+++ src/main/java/com/mazdak/CalculatorApp.java
@@ -1,7 +1,40 @@
 package com.mazdak;
 
+import java.util.Scanner;
+
 public class CalculatorApp {
     public static void main(String[] args){
-
+        Scanner input = new Scanner(System.in);
+        boolean inMain = true;
+        while (inMain){
+            System.out.println("******************[ Main Menu ]*******************");
+            System.out.println("*     1. Simple Operations (Two Numbers)         *");
+            System.out.println("*     2. Advanced Operations (List of Numbers)   *");
+            System.out.println("*     3. Show History                            *");
+            System.out.println("*     4. Clear History                           *");
+            System.out.println("*     0. Exit                                    *");
+            System.out.println("**************************************************");
+            String selected = input.nextLine();
+            switch (selected){
+                case "1":
+                    System.out.println("*******[ Simple Operations (Two Numbers) ]********");
+                    break;
+                case "2":
+                    System.out.println("****[ Advanced Operations (List of Numbers) ]*****");
+                    break;
+                case "3":
+                    System.out.println("*******************[ History ]********************");
+                    break;
+                case "4":
+                    System.out.println("*********[ History completely cleared. ]**********");
+                    break;
+                case "0":
+                    System.out.println("********************[ FINISH ]********************");
+                    inMain = false;
+                    break;
+                default:
+                    System.out.println("****************[ Invalid Option ]****************");
+            }
+        }
     }
 }
